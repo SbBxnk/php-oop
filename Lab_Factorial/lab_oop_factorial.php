67144060e0c87033ffa3e133fcd6c14a7cc41d2d
chayakorn phukhiao <chayakorn.code@gmail.com>
2024-09-20 12:02:07 +0700

    Factorail

--- Lab_Factorial/lab_oop_factorial.php
+++ Lab_Factorial/lab_oop_factorial.php
@@ -1,46 +1,96 @@
-<?php // คลาสแม่ MathOperation
+<?php 
 class MathOperation
 {
     protected $number;
-    // คอนสตรัคเตอร์เพื่อรับค่าตัวเลข
+
     public function __construct($number)
     {
         $this->number = $number;
     }
-    // เมธอด calculate ที่จะถูกเขียนทับในคลาสลูก
+
     public function calculate()
     {
         return "No calculation defined in MathOperation class.";
     }
 }
 
-    class factorial extends MathOperation{
-        
-        public function calculate()
-        {
-            $F =1 ;
-            $K = $this->number;
-            while($K>1){
-                $F = $F * $K;
-                $K = $K-1;
-            }
-            return $F;
+class factorial extends MathOperation
+{
+
+    public function calculate()
+    {
+        $F = 1;
+        $K = $this->number;
+        while ($K > 1) {
+            $F = $F * $K;
+            $K = $K - 1;
         }
-    
+        return $F;
     }
+}
+
+class Rec_factorial extends MathOperation
+{
+    public function calculate(){
+        return $this->cal_factorial($this->number);
+    }
+    private function cal_factorial($N){
+        if ($N == 0 || $N == 1)
+            return 1;
+        else
+            return $N * $this->cal_factorial($N - 1);
+    }
+}
+class Square extends MathOperation{
+    public function calculate(){
+        return $this->number * $this->number;
+    }
+}
+class Circle extends MathOperation{
+    public function calculate(){
+        // return pi() *  pow($this->number,2);
+        return pi() *  $this->number **2;
+    }
+}
 
 ?>
 
-<h2>เรียกใช้ class</h2>
- <?php 
-    $N=5;
-    $fac = new MathOperation($N);
-    $fac_result = $fac->calculate();
+<h4>เรียกใช้ class</h4>
+<?php
+$N = 5;
+$fac = new MathOperation($N);
+$fac_result = $fac->calculate();
 
-    $fac1 = new factorial($N);
-    $fac_result1 = $fac1->calculate();
- ?>
+$fac = new factorial($N);
+$fac1 = $fac->calculate();
 
- <p><?php echo $fac_result ?></p>
- <hr>
- <p><?php echo "$N! =". $fac_result1 ?></p>
+$fac = new Rec_factorial($N);
+$fac2 = $fac->calculate();
+
+$sqaure = new Square($N);
+$sqaure1 = $sqaure->calculate();
+
+$circle = new Circle($N,null);
+$circle1 = $circle->calculate();
+?>
+
+<p><?php echo $fac_result ?></p>
+<hr>
+<p><?php echo "$N! =" . $fac1 ?></p>
+<h4>Recursive Function</h4>
+<p><?php echo "$N! =" . $fac2 ?></p>
+<h4>หาพื้นที่สี่เหลี่ยมจตุรัส</h4>
+<p><?php echo "$N x $N =" . $sqaure1 ?> ตร.หน่วย</p>
+<h4>หาพื้นที่วงกลม</h4>
+<p><?php echo "3.14 x $N<sup>2</sup> =" . number_format($circle1,2) ?> ตร.หน่วย</p>
+
+
+<h2 class="text-danger fw-bold ">Home workdadadadada</h2>
+
+<pre>
+select 
+    - Fac OOP
+    - Recursive
+    - sqaure
+    - Circle
+</pre>
